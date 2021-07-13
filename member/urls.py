aa17c98b787de364818c2b1190bd4e0fe2da837f
whdtjdgld@naver.com <whdtjdgld@naver.com>
2021-07-13 21:34:22 +0900

    폰트, 메인수정, 로긴회갑 완료

--- member/urls.py
+++ member/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from member import views
 
 urlpatterns = [
+    path("signup", views.signup, name="signup"),
     path("", views.main, name="main"),
     path("main", views.main, name="main"),
     path("profile", views.profile, name="profile"),
     path("login", views.login, name="login"),
     path("signup", views.signup, name="signup"),
-    
-    
+    path("q2", views.q2, name="q2"),
+    path("q3", views.q3, name="q3"),
+    path("q4", views.q4, name="q4"),
 ]
